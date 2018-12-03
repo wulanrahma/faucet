695e57bd87409cf65864c879179d06b345c53cb1
wulanrahma <45566266+wulanrahma@users.noreply.github.com>
2018-12-03 21:32:38 +0700

    Add files via upload

---
+++ faucet.js
@@ -0,0 +1,226 @@
+var s=[
+"https://freebitco.in/?r=673111",
+"http://frenchbitgirl.space/?r=2334",
+"http://claimbtc.com/?r=62ae7a2317",
+"http://coin64.com/home/aKSZqg==",
+"https://adbit.co/?r=IFmZvxosnGctyGTd6Hsbv84ZawqCFK",
+"http://www.bitcoinaliens.com/faucet?ref=nonfb-56bcd3909ab1d",
+"http://www.siizo.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.quickfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.btcfixer.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.foxfaucet.com/?id=2201295",
+"http://coin64.com/home/aKSZqg==",
+"http://www.boxfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"https://adbit.co/?r=IFmZvxosnGctyGTd6Hsbv84ZawqCFK",
+"http://www.siizo.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://btcboom.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://givemebtc.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.bitcoinaliens.com/faucet?ref=nonfb-56bcd3909ab1d",
+"http://yourbtc.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://fastcoin.ml/bitcoin/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://earthbitco.in/?ref=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"https://adbit.co/?r=IFmZvxosnGctyGTd6Hsbv84ZawqCFK",
+"http://free-bitcoin.org/index.php?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.goldfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://fastbtc.net/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://DreamFaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://mybestbitcoin.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.ninjafaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://starsbit.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.bitcoinaliens.com/faucet?ref=nonfb-56bcd3909ab1d",
+"http://coin64.com/home/aKSZqg==",
+"http://freebitcorn.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://easybitcoinfaucet.com/?r=3406e35750",
+"https://adbit.co/?r=IFmZvxosnGctyGTd6Hsbv84ZawqCFK",
+"http://takefreebitcoin.com/?r=5d4936d33e",
+"http://claimbtc.com/?r=62ae7a2317",
+"http://www.siizo.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://givecoins.net/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.bitstap.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.quickfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://moonclaim.co.in/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.bitcoinerz.eu/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.btcfixer.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://btcboom.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://coin64.com/home/aKSZqg==",
+"http://givemebtc.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://bonusbitcoin.co/?ref=0455D63AE593",
+"http://www.btcfixer.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://bigfaucet.ml/?f=1&r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"https://adbit.co/?r=IFmZvxosnGctyGTd6Hsbv84ZawqCFK",
+"http://www.foxfaucet.com/?id=2201295",
+"http://www.bitcoinaliens.com/faucet?ref=nonfb-56bcd3909ab1d",
+"http://yourbtc.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://freebitco.in/?r=2111968",
+"http://DreamFaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://winbit.ga/home/U-lco1P32Ich1gfnt3HqxbDyI7G5FO4mZr2LXN4iH9Y",
+"http://mybestbitcoin.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://fastbtc.net/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.ninjafaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://starsbit.com/freebitcoin?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://spinandwinonline.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://faucetbitcoin.net/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.globalfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://coin64.com/home/aKSZqg==",
+"http://www.goldfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://starsbit.com/freebitcoin?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.quickfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://moonclaim.co.in/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.bitcoinerz.eu/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"https://adbit.co/?r=IFmZvxosnGctyGTd6Hsbv84ZawqCFK",
+"http://claimbtc.com/?r=62ae7a2317",
+"http://www.siizo.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.bluebitcoin.ru/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.satoshicity.org/?r=138162",
+"http://btcboom.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://playbitco.in/?ref=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.MillionSatoshi.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://bonusbitcoin.co/?ref=0455D63AE593",
+"http://www.goldfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.crownfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.FreeCryptoCoin.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.btcfixer.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://bigfaucet.ml/?f=1&r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.foxfaucet.com/?id=2201295",
+"http://coin64.com/home/aKSZqg==",
+"http://www.lovebitcoin.eu/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"https://adbit.co/?r=IFmZvxosnGctyGTd6Hsbv84ZawqCFK",
+"http://starsbit.com/freebitcoin?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://takefreebitcoin.com/?r=5d4936d33e",
+"http://claimbtc.com/?r=62ae7a2317",
+"http://www.goldfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://zoobit.co.in/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://yourbtc.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://rekbitcoin.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://fastbtc.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://minions-faucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.pickupcoin.com/btc/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://greenbitco.in/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://aquabitcoin.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://coin64.com/home/aKSZqg==",
+"http://easybitcoinfaucet.com/?r=3406e35750",
+"http://www.bitcoinaliens.com/faucet?ref=nonfb-56bcd3909ab1d",
+"http://fastbtc.net/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.valley-bitcoin.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.takeourbtc.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://ifaucet.info/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos#sthash.cwETeqOt.dpuf",
+"http://freebitcoinhome.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"https://adbit.co/?r=IFmZvxosnGctyGTd6Hsbv84ZawqCFK",
+"http://rainingfreebitcoins.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://givecoins.net/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://faucetsfree.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://earnbitcoinonline.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.drawcoins.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.babyfreebtc.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.casinofaucetbitc.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://neobitcoinfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://bigfaucet.ml/?f=1&r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://freesatoshi.com.ua/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://earthbitco.in/?ref=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.freebtc.us/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://btcboom.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://coin64.com/home/aKSZqg==",
+"http://givemebtc.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.faucetgoldtime.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://kingofmachines.net/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://spinandwinonline.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://freebitcoins.online/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://printmoney.info/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://freecoins.info/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://faucet.jokertimes.co/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://yourfaucet.net/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://bitcoincoin.online/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.foxfaucet.com/?id=2201295",
+"https://adbit.co/?r=IFmZvxosnGctyGTd6Hsbv84ZawqCFK",
+"http://faucetix.ru/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://starsbit.com/freebitcoin?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://takefreebitcoin.com/?r=5d4936d33e",
+"http://www.goldfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.mrchocobit.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://claimfaucet.info/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://coin64.com/home/aKSZqg==",
+"http://faucetcomic.info/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://rainingfreebitcoins.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://faucetsgalaxy.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://bluesatoshi.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://bonusbitcoin.co/?ref=0455D63AE593",
+"http://razercrypt.com/btc/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://xchange.cc/bitcoincran/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://btc4me.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://faucet.bitcoins43.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://bitcoincoin.online/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.breaktheco.in/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://theearner.co.in/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://claimbtc.com/?r=62ae7a2317",
+"http://coin64.com/home/aKSZqg==",
+"http://fastbtc.net/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.get-free-bitco.in/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://yourbtc.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.goldfaucet.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://hentenaar.com/faucet?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://coin64.com/home/aKSZqg==",
+"http://claimbtc.com/?r=62ae7a2317",
+"http://www.bitcoinaliens.com/faucet?ref=nonfb-56bcd3909ab1d",
+"http://thegurucoin.com/index.php?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://winbit.ga/home/U-lco1P32Ich1gfnt3HqxbDyI7G5FO4mZr2LXN4iH9Y",
+"http://freebit.mine.nu/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://faucet.jokertimes.co/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://coin64.com/home/aKSZqg==",
+"http://bitcoinsblue.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://icebitco.in/?ref=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://btc-kit.name/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.bitcoinaliens.com/faucet?ref=nonfb-56bcd3909ab1d",
+"http://earthbitco.in/?ref=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://e-satoshi.com/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://www.satoshiismoney.eu/btc/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://btcboom.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://givemebtc.xyz/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://easybitcoinfaucet.com/?r=3406e35750",
+"http://givecoins.net/?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"www.faucetfreebitcoin.com//math.php?r=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos",
+"http://adsroyale.com/page/landing/1/307",
+"http://www.daddycoins.com/?referer=1Aqw5KqvSByNCZf4DwcPeRCM8DkDpA7cos"
+
+"http://takefreebitcoin.com/?r=5d4936d33e"
+];
+var adr,i,x=0,c=s.length;
+function address() {
+adr=prompt('Enter your bitcoin address:');
+s=s.map(function(x){
+  return x.indexOf("a=") != -1 ? x + adr : x; 
+});
+}
+function next(){
+x+=1;
+if (x>c-1)
+  {
+  x=0;
+  }
+changeSrc();
+}
+function prev(){
+x-=1;
+if (x<=0)
+  {
+  x=c-1;
+  }
+changeSrc();
+}
+function jumpTo() {
+i=prompt("Enter a number to skip to that faucet.");
+x=0;
+while (i!=x) {
+  if (x>c-2) {
+  break;
+  }
+  x+=1;
+}
+changeSrc();
+}
+function newTab() {
+var win=window.open(document.getElementById("fm").src, '_blank');
+win.focus();
+}
+function changeSrc() {
+document.getElementById("fm").src=s[x];
+}
